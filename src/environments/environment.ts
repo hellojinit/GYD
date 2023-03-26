cc9a06f5162e25d5acf79d3129618e7c50a921b5
Jinit Shah <jinit@renee.app>
2023-03-26 19:29:44 -0400

    basecode

---
+++ src/environments/environment.ts
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+//
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBZ7C8aoBJ4wjgfkTdfF7MuilKKkmuz5S4",
+//   authDomain: "gydapp-b8dd4.firebaseapp.com",
+//   projectId: "gydapp-b8dd4",
+//   storageBucket: "gydapp-b8dd4.appspot.com",
+//   messagingSenderId: "894621565915",
+//   appId: "1:894621565915:web:f3bfe7d711b3ec42d2ddfe"
+// };
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBZ7C8aoBJ4wjgfkTdfF7MuilKKkmuz5S4",
+    authDomain: "gydapp-b8dd4.firebaseapp.com",
+    projectId: "gydapp-b8dd4",
+    storageBucket: "gydapp-b8dd4.appspot.com",
+    messagingSenderId: "894621565915",
+    appId: "1:894621565915:web:f3bfe7d711b3ec42d2ddfe"
+  }
+};
+//
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
