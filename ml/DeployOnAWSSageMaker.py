f404121e665f954ac54bc233e7b58640a7e3aa73
Mox301 <88712877+Mox301@users.noreply.github.com>
2023-03-31 01:44:28 +0200

    Add files via upload
    Deploy Google Pegasus Model on AWS SageMaker

---
+++ ml/DeployOnAWSSageMaker.py
@@ -0,0 +1,28 @@
+from sagemaker.huggingface import HuggingFaceModel
+import sagemaker
+
+role = sagemaker.get_execution_role()
+# Hub Model configuration. https://huggingface.co/models
+hub = {
+	'HF_MODEL_ID':'google/pegasus-large',
+	'HF_TASK':'summarization'
+}
+
+# create Hugging Face Model Class
+huggingface_model = HuggingFaceModel(
+	transformers_version='4.17.0',
+	pytorch_version='1.10.2',
+	py_version='py38',
+	env=hub,
+	role=role, 
+)
+
+# deploy model to SageMaker Inference
+predictor = huggingface_model.deploy(
+	initial_instance_count=1, # number of instances
+	instance_type='ml.m5.xlarge' # ec2 instance type
+)
+
+predictor.predict({
+	'inputs': "The tower is 324 metres (1,063 ft) tall, about the same height as an 81-storey building, and the tallest structure in Paris. Its base is square, measuring 125 metres (410 ft) on each side. During its construction, the Eiffel Tower surpassed the Washington Monument to become the tallest man-made structure in the world, a title it held for 41 years until the Chrysler Building in New York City was finished in 1930. It was the first structure to reach a height of 300 metres. Due to the addition of a broadcasting aerial at the top of the tower in 1957, it is now taller than the Chrysler Building by 5.2 metres (17 ft). Excluding transmitters, the Eiffel Tower is the second tallest free-standing structure in France after the Millau Viaduct."
+})
