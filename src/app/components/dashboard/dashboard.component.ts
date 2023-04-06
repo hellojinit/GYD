72ed21ee6e7189195de1e5ef226b45c769ee1f3e
Shalini <shalinidubey1@yahoo.com>
2023-04-06 17:54:16 -0400

    Putting model into Dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../shared/services/auth.service';
+import { HttpClient } from "@angular/common/http";
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
 })
 
-export class DashboardComponent implements OnInit {
-  constructor(public authService: AuthService) {}
-  ngOnInit(): void {}
+export class DashboardComponent implements OnInit{
+
+  // @ts-ignore
+  textInput: string;
+  // @ts-ignore
+  textOutput: string;
+
+  constructor(
+    public authService: AuthService
+    // public http: HttpClient
+  ) {}
+  //
+  ngOnInit(): void {
+
+  }
+  //
+  onSubmit() {
+    const url = `https://text-summarization-model-jvgcexcekq-uc.a.run.app/summarize/`;
+    // this.http.get(url).subscribe((response: any) => {
+    //   console.log(response);
+  //     // Do something with the
+  //     // this.textOutput = response.data;
+  //   });
+    this.textOutput = "helloooo"
+  }
 }
