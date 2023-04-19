8570254ba69efc8626a802b046759015cfcee08b
Jinit Shah <jinit@renee.app>
2023-04-18 22:57:33 -0400

    Added auto typing texts and user history.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,41 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {NgxTypedJsModule} from 'ngx-typed-js';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { AngularFireStorageModule } from '@angular/fire/compat/storage';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
-import { environment } from '../environments/environment';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import {AngularFireModule} from '@angular/fire/compat';
+import {AngularFireAuthModule} from '@angular/fire/compat/auth';
+import {AngularFireStorageModule} from '@angular/fire/compat/storage';
+import {AngularFirestoreModule} from '@angular/fire/compat/firestore';
+import {AngularFireDatabaseModule} from '@angular/fire/compat/database';
+import {environment} from '../environments/environment';
 
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { SignInComponent } from './components/sign-in/sign-in.component';
-import { SignUpComponent } from './components/sign-up/sign-up.component';
-import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
-import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {SignInComponent} from './components/sign-in/sign-in.component';
+import {SignUpComponent} from './components/sign-up/sign-up.component';
+import {ForgotPasswordComponent} from './components/forgot-password/forgot-password.component';
+import {VerifyEmailComponent} from './components/verify-email/verify-email.component';
 
-import { AuthService } from "./shared/services/auth.service";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AuthService} from "./shared/services/auth.service";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule} from "@angular/material/button";
 import {MatInputModule} from "@angular/material/input";
 import {MatCardModule} from "@angular/material/card";
-import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import {UserProfileComponent} from './components/user-profile/user-profile.component';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
 import {FormsModule} from "@angular/forms";
-import { KnowledgeGraphComponent } from './components/knowledge-graph/knowledge-graph.component';
-import { HomeComponent } from './components/home/home.component';
-import { LottieModule } from 'ngx-lottie';
-import player from 'lottie-web';
+import {KnowledgeGraphComponent} from './components/knowledge-graph/knowledge-graph.component';
+import {HomeComponent} from './components/home/home.component';
+// import {LottieModule} from 'ngx-lottie';
 
-export function playerFactory(): any {
-  return import('lottie-web');
-}
+// export function playerFactory(): any {
+//   return import('lottie-web');
+// }
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -62,9 +64,12 @@ export function playerFactory(): any {
     MatToolbarModule,
     MatIconModule,
     FormsModule,
-    LottieModule.forRoot({ player: playerFactory }),
+    FlexLayoutModule,
+    NgxTypedJsModule
+    // LottieModule.forRoot({player: playerFactory}),
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../shared/services/auth.service';
 import { HttpClient } from "@angular/common/http";
 
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -15,34 +20,59 @@ export class DashboardComponent implements OnInit{
   textInput: string;
   // @ts-ignore
   textOutput: string;
-
   loading: boolean = false;
-  // @ts-ignore
-  file: File = null;
+  fileSelected = false;
+
+  userInputsOutputsCollection: AngularFirestoreCollection<any>; // Firestore collection reference
+  userInputsOutputs: Observable<any[]>; // Observable for user inputs and outputs data
 
   constructor(
     public authService: AuthService,
-    private http: HttpClient
-  ) {}
+    private http: HttpClient,
+    private firestore: AngularFirestore
+  ) {
+    this.userInputsOutputsCollection = this.firestore.collection('users').doc(this.authService.userData.uid).collection('History'); // Reference to "userInputsOutputs" collection in Firestore
+    this.userInputsOutputs = this.userInputsOutputsCollection.snapshotChanges().pipe(
+      map(actions => {
+        // Map the snapshot changes to an array of user inputs and outputs data
+        return actions.map(a => {
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id;
+          return { id, ...data };
+        }).sort((a, b) => {
+          // Sort the data array by date and time in descending order
+          return b.dateTime.toDate().getTime() - a.dateTime.toDate().getTime();
+        });
+      })
+    );
+
+  }
   //
   ngOnInit(): void {
 
   }
   onChange(event: any) {
     console.log(event.target.files[0])
-    this.file = event.target.files[0]
+    this.fileSelected = true;
+    const file = event.target.files[0]; // Get the selected file
+    const reader = new FileReader(); // Create a FileReader object
+
+    // Register an onload event handler for when the file is loaded
+    reader.onload = (e: any) => {
+      const fileContents = e.target.result; // Get the file contents as a string
+      console.log(fileContents); // Do whatever you want with the file contents
+      this.textInput = fileContents;
+    };
+    // Read the file as text
+    reader.readAsText(file);
   }
   onUpload() {
-    console.log(this.file);
-    if (this.file) {
+    console.log(this.fileSelected);
+    if (this.fileSelected) {
       alert("Uploaded")
-      // const reader = new FileReader();
-      // reader.readAsText(this.file);
-      // console.log(reader.result);
-    } else {
-      this.loading = !this.loading
-      // alert("Please select a file first")
+      this.fileSelected = false
     }
+    this.loading = !this.loading
   }
 
   onSubmit() {
@@ -52,6 +82,58 @@ export class DashboardComponent implements OnInit{
     this.http.get(url).subscribe((response: any) => {
       console.log(response);
       this.textOutput = response.summary;
+      this.saveUserInputOutput(this.textInput, this.textOutput);
     });
+  }
+
+  saveUserInputOutput(input: string, output: string) {
+    const dateTime = new Date();
+    // Create a new document with a unique ID in the "userInputsOutputs" collection
+    this.userInputsOutputsCollection.add({
+      input: input,
+      output: output,
+      dateTime: dateTime
+    })
+      .then(() => {
+        console.log('User input and output saved to Firestore successfully.');
+      })
+      .catch((error) => {
+        console.error('Error saving user input and output to Firestore: ', error);
+      });
+  }
+
+  fetchUserHistory() {
+    this.userInputsOutputs.subscribe((historyData: any[]) => {
+      console.log('User history data:', historyData);
+      // Do whatever you want with the user history data, e.g., update a local array to display in the UI
+    });
+  }
+  deleteUserInputOutput(itemId: string) {
+    // Delete the document from Firestore using the document ID
+    this.userInputsOutputsCollection.doc(itemId).delete()
+      .then(() => {
+        console.log('User input and output deleted from Firestore successfully.');
+      })
+      .catch((error) => {
+        console.error('Error deleting user input and output from Firestore: ', error);
+      });
+  }
+
+  summarizeAgain(itemInput: string) {
+    this.textInput = itemInput
+    this.onSubmit()
+  }
+
+  copyOutput(output: string): void {
+    // Create a new text area element to hold the output text
+    const textarea = document.createElement('textarea');
+    textarea.value = output;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy'); // Copy the text to clipboard
+    document.body.removeChild(textarea); // Remove the temporary text area element
+
+    // Show a notification or feedback to indicate that the text has been copied
+    alert('Output text has been copied to clipboard!');
   }
 }
