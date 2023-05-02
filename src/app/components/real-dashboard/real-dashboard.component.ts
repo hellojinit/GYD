da4b3edf779966eb24b27b00a0ec5465d760421a
Shalini <shalinidubey1@yahoo.com>
2023-05-02 00:19:55 -0400

    DONE

--- src/app/components/real-dashboard/real-dashboard.component.ts
+++ src/app/components/real-dashboard/real-dashboard.component.ts
@@ -17,24 +17,7 @@ export class RealDashboardComponent {
   }
 
   ngOnInit(): void {
-    this.slides[0] = {
-      id: 0,
-      src: './assets/text-sum.png',
-      title: 'Text Summarization',
-      subtitle: 'Upload, write, or paste any text and GYD will summarize it in moments.'
-    };
-    this.slides[1] = {
-      id: 1,
-      src: './assets/KG.png',
-      title: 'Knowledge Graph',
-      subtitle: 'Upload, write, or paste any text and GYD will give you a knowledge graph.'
-    }
-    // this.slides[2] = {
-    //   id: 2,
-    //   src: './assets/KG.png',
-    //   title: 'Q & A',
-    //   subtitle: 'Discover GYD\'s new feature to answer your questions.'
-    // }
+
   }
 
   onItemChange($event: any): void {
